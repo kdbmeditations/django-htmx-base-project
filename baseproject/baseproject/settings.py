e1abe34d4304376b4c7557dd3804d95baff06cc5
kaldbmeditations <k.d.balabanov@gmail.com>
2023-08-01 22:33:06 +0100

    Added django-htmx, some starting templates, main app, requirements.txt

--- baseproject/baseproject/settings.py
+++ baseproject/baseproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_htmx',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'baseproject.urls'
