f093bda163f6cbe40c06045ea8a1f675512bf38b
kaldbmeditations <k.d.balabanov@gmail.com>
2023-08-05 23:20:17 +0100

    Added black as part of pre-commit step for formatting, added updated requirements.txt

--- baseproject/main/views.py
+++ baseproject/main/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
+
 def index(request):
     if request.htmx:
         print("Received HTMX request.")
-        return render(request, 'partial.html')
+        return render(request, "partial.html")
     else:
         print("Received non-HTMX request.")
-        return render(request, 'index.html')
+        return render(request, "index.html")
